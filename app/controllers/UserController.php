ed4cfebd51282ff3ce210a01f594c0022c70896c
Lester Cordero Murillo <lestercorderomurillo@gmail.com>
2022-02-23 22:39:01 -0600

    Update Fix

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -470,11 +470,11 @@ class UserController extends Controller
         }
 
         $books = [
-            ["*", Language::getString("misc25") . " " . $userInfo->firstName . " " . $userInfo->lastName],
-            ["*", Language::getString("birthDay") . " " . $userInfo->birthDay],
-            ["*", Language::getString("country") . " " . $userInfo->country],
-            ["*", Language::getString("phone") . " " . $userInfo->phone],
-            ["*", Language::getString("lookup012") . " " . $userInfo->gender],
+            ["*", Language::getString("misc25") . ": " . $userInfo->firstName . " " . $userInfo->lastName],
+            ["*", Language::getString("birthDay") . ": " . $userInfo->birthDay],
+            ["*", Language::getString("country") . ": " . $userInfo->country],
+            ["*", Language::getString("phone") . ": " . $userInfo->phone],
+            ["*", Language::getString("lookup012") . ": " . $userInfo->gender],
             [],
         ];
 
